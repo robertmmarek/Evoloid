3c9a88f74a6bb358a0ae42e1703d1b37f0279cfc
rmarek <rmarek@tenneco.com>
2017-08-24 14:22:14 +0200

    Refractored to use View

--- Evoloid.py
+++ Evoloid.py
@@ -2,56 +2,96 @@ import pygame as pg
 import numpy as np
 import random as rnd
 
+class View(pg.sprite.Sprite):
 
-class CosmicSprite(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+
+    def update_sprite(self, scale=1.):
+        pass
+
+    def draw(self, screen):
+        pass
+
+class CosmicObjectImageView(View):
+    def __init__(self, cosmic_object, image):
+        View.__init__(self)
+        self.cosmic_object = cosmic_object
+        self._base_image = image
+        self.image = image
+        self.rect = self.image.get_rect()
+
+    def update_sprite(self, scale=1.):
+        View.update_sprite(scale)
+
+        angle_in_deg = np.rad2deg(self.cosmic_object.angle)
+        previous_center = (self.cosmic_object.position[0]*scale, self.cosmic_object.position[1]*scale)
+        self.image = pg.transform.rotate(self._base_image, angle_in_deg)
+        image_rect = self.image.get_rect()
+        base_image_rect = self._base_image.get_rect()
+        max_dimension = max(base_image_rect.width, base_image_rect.height)
+        resize_factor = 2*self.cosmic_object.radius*scale/max_dimension
+        self.image = pg.transform.scale(self.image, (int(image_rect.width*resize_factor),
+                                                     int(image_rect.height*resize_factor)))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = previous_center
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class CosmicObject(pg.sprite.Sprite):
+    @staticmethod
+    def collision(cosmic_object_a, cosmic_object_b):
+        ret = pg.sprite.collide_circle(cosmic_object_a, cosmic_object_b)
+        if ret:
+            cosmic_object_a.on_collision(cosmic_object_b)
+            cosmic_object_b.on_collision(cosmic_object_a)
+
+        return ret
+
     def __init__(self,
-                 sprite_img,
                  mass=1.,
                  velocity=[0., 0.],
                  position=[0., 0.],
                  angular_velocity=0.,
                  angle=0.,
                  friction=.2,
+                 radius = 1.
                  ):
         pg.sprite.Sprite.__init__(self)
-        self._base_image = sprite_img
-        self.image = sprite_img
-        self.rect = self.image.get_rect()
         self.mass = mass
         self.velocity = np.array(velocity)
         self.angular_velocity = angular_velocity
         self.position = np.array(position)
-<<<<<<< HEAD
-        self.angle = np.array(angle)
-        #fix that
-        self.radius = max(self.rect)
-=======
+        self.rect = pg.Rect(0, 0, 1., 1.)
+
         self.angle = angle
-        self.radius = max(self.rect.width, self.rect.height)
+        self.radius = radius
         self.friction = friction
         self._position_before_update = self.position[:]
         self._angle_before_update = angle
+
         self._update_rect()
 
-
-    def _update_rect(self):
-        self.rect.left = self.position[0] - self.rect.width/2.
-        self.rect.top = self.position[1] - self.rect.height/2.
-
     def on_collision(self, other):
-        if isinstance(other, CosmicSprite):
-            self.velocity *= (self.friction*other.friction)
-            self.angular_velocity *= (self.friction*other.friction)
+        if isinstance(other, CosmicObject):
+            self.velocity = 0.
+            self.angular_velocity = 0.
             self.position = self._position_before_update
->>>>>>> fa5dc2c8c13ea4418f8068a89457bfffdf3306e2
 
     def update_gravity(self, dt=.04, gravity_objects=[]):
         for gravity_object in gravity_objects:
             object_position = gravity_object.position
             gravity_vector = object_position-self.position
             gravity_distance = np.linalg.norm(gravity_vector)
-            gravity_acceleration = float('6.7e-11')*gravity_object.mass/(gravity_distance**2)
-            self.velocity += gravity_acceleration*dt
+            normalized_gravity_vector = gravity_vector/gravity_distance
+            gravity_acceleration = float('6.7e-3')*gravity_object.mass/(gravity_distance**2)
+            self.velocity += normalized_gravity_vector*gravity_acceleration*dt
+
+    def _update_rect(self):
+        self.rect.center = (self.position[0], self.position[1])
 
     def update(self, dt=.04):
         pg.sprite.Sprite.update(self)
@@ -60,27 +100,27 @@ class CosmicSprite(pg.sprite.Sprite):
         self.position = self.position + self.velocity*dt
         angle_increment = self.angular_velocity*dt
         self.angle = (self.angle + angle_increment) % (2*np.pi)
-
-        self.image = pg.transform.rotate(self._base_image, np.rad2deg(self.angle))
         self._update_rect()
 
-class CosmicSpriteDecorator(CosmicSprite):
-    def __init__(self, cosmic_sprite):
-        self.cosmic_sprite = cosmic_sprite
 
-class Ship(CosmicSpriteDecorator):
+class CosmicObjectDecorator(CosmicObject):
+    def __init__(self, cosmic_object):
+        self.cosmic_object = cosmic_object
+
+
+class Ship(CosmicObjectDecorator):
     angular_thruster_cost = 1.
     position_thruster_cost = 1.
     crash_fuel_cost = 1.
     crash_threshold = 1.
 
     def __init__(self,
-                 cosmic_sprite,
+                 cosmic_object,
                  angular_thrusters=0.,
                  position_thrusters=0.,
                  fuel=100.
                  ):
-        CosmicSpriteDecorator.__init__(self, cosmic_sprite)
+        CosmicObjectDecorator.__init__(self, cosmic_object)
         self.angular_thrusters = angular_thrusters
         self.position_thrusters = position_thrusters
         self.fuel = fuel
@@ -89,7 +129,7 @@ class Ship(CosmicSpriteDecorator):
         if item in self.__dict__.keys():
             return getattr(self, item)
         else:
-            return getattr(self.cosmic_sprite, item)
+            return getattr(self.cosmic_object, item)
 
     def on_collision(self, other):
         velocity_diff = np.linalg.norm(self.velocity - other.velocity)
@@ -97,11 +137,11 @@ class Ship(CosmicSpriteDecorator):
         crash_damage = max(0., energy - Ship.crash_threshold)
         self.fuel -= crash_damage*Ship.crash_fuel_cost
         self.fuel = max(0., self.fuel)
-        CosmicSprite.on_collision(self, other)
+        self.cosmic_object.on_collision(other)
 
 
     def update_gravity(self, dt=.04, gravity_objects=[]):
-        self.cosmic_sprite.update_gravity(dt, gravity_objects)
+        self.cosmic_object.update_gravity(dt, gravity_objects)
 
     def update(self, dt=.04):
         position_thrust = self.position_thrusters*dt
@@ -110,9 +150,9 @@ class Ship(CosmicSpriteDecorator):
         angular_thrust = self.angular_thrusters*dt
         self.fuel -= max(0., np.abs(angular_thrust)*Ship.angular_thruster_cost)
 
-        origin = self.cosmic_sprite.position
+        origin = self.cosmic_object.position
         direction = np.array([0., 1.])
-        angle = self.cosmic_sprite.angle
+        angle = self.cosmic_object.angle
         rotated_direction = np.array([direction[0]*np.cos(angle) - direction[1]*np.sin(angle),
                                       direction[1]*np.cos(angle) + direction[0]*np.sin(angle)]).reshape(2)
 
@@ -121,14 +161,15 @@ class Ship(CosmicSpriteDecorator):
         angular_velocity_inc = angular_thrust
 
         if self.fuel > 0.:
-            self.cosmic_sprite.velocity += position_velocity_inc
-            self.cosmic_sprite.angular_velocity += angular_velocity_inc
+            self.cosmic_object.velocity += position_velocity_inc
+            self.cosmic_object.angular_velocity += angular_velocity_inc
 
-        self.cosmic_sprite.update(dt)
+        self.cosmic_object.update(dt)
+
 
 def main():
     pg.init()
-    screen_size = (640, 320)
+    screen_size = (640, 640)
     screen = pg.display.set_mode(screen_size)
 
     images_dict = {
@@ -136,31 +177,43 @@ def main():
         'planet': pg.image.load(r'./img/planet.png')
     }
 
-    ships = pg.sprite.Group()
-    ships.add(Ship(CosmicSprite(sprite_img=images_dict['ship'],
-                                position=[30., 30.],
-                                angular_velocity=0.1)))
-    planets = pg.sprite.Group()
-    planets.add(CosmicSprite(sprite_img=images_dict['planet'],
-                             position=[screen_size[0]/2,
-                                       screen_size[1]/2],
-                             angular_velocity=0.05),
-                             )
+    ships = [Ship(CosmicObject(position=[320., 100.],
+                               radius=10.,
+                               angular_velocity=0.005,
+                               velocity=[0., 0.]))]
+    planets = [CosmicObject(position=[320., 320.],
+                            radius=20.,
+                            mass=500.,
+                            angular_velocity=0.005)]
 
-    clock = pg.time.Clock()
+    ships_view = [CosmicObjectImageView(ship, images_dict['ship']) for ship in ships]
+    planets_view = [CosmicObjectImageView(planet, images_dict['planet']) for planet in planets]
+
+    global_clock = pg.time.Clock()
 
     while True:
-        screen.fill((0,0,0))
-        ships.draw(screen)
-        planets.draw(screen)
+        screen.fill((0, 0, 0))
+        dt = global_clock.tick(30)
 
-        ships.update(0.04)
-        planets.update(0.04)
+        for ship in ships:
+            ship.update_gravity(dt, planets)
+            ship.update(dt)
+
+        for planet in planets:
+            planet.update(dt)
+
+        collide_group = ships+planets
+        collide_group_len = len(collide_group)
+        for i in range(0, collide_group_len):
+            element_a = collide_group.pop(0)
+            for element_b in collide_group:
+                CosmicObject.collision(element_a, element_b)
+
+        for view in ships_view+planets_view:
+            view.update_sprite(scale=1.)
+            view.draw(screen)
 
         pg.display.update()
-
-        clock.tick(24)
-
 
 
 if __name__ == '__main__':
